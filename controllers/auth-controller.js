3a6473c4ce14de1417b06c7f60603ef078b6ae87
sargerock <sargerock.ua@gmail.com>
2020-04-01 16:45:23 +0300

    auth-controller errors

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,7 +6,7 @@ import { ValidationError, AuthorizationError } from "../utils/errors";
 import { createTokens, createToken } from "../utils";
 import { EXPIRES_ACCESS } from "../config";
 
-export const signUp = async (req, res, next) => {
+export const signUp = async (req, res) => {
 	const { userName, email, password } = req.body;
 
 	const user = await User.create({
@@ -21,12 +21,12 @@ export const signUp = async (req, res, next) => {
 	res.status(201).json({ accessToken, refreshToken });
 };
 
-export const signIn = async (req, res, next) => {
+export const signIn = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ where: { email } });
-	if (!user) throw new ValidationError("Incorrect email or password");
+	if (!user) throw new ValidationError(null, "Incorrect email or password");
 	const match = await bcrypt.compare(password, user.password);
-	if (!match) throw new ValidationError("Incorrect email or password");
+	if (!match) throw new ValidationError(null, "Incorrect email or password");
 
 	const { accessToken, refreshToken } = createTokens({ id: user.id });
 	Auth.create({ refreshToken, accessToken });
@@ -34,32 +34,27 @@ export const signIn = async (req, res, next) => {
 	res.status(201).json({ accessToken, refreshToken });
 };
 
-export const signOut = async (req, res, next) => {
+export const signOut = async (req, res) => {
 	const { accessToken } = req;
 
 	const auth = await Auth.findOne({ where: { accessToken } });
-	if (!auth) {
-		next(new HandledError("Not found", 404));
-		return;
-	}
+	if (!auth) throw new HandledError("Not found", 404);
+
 	auth.destroy();
 
 	res.status(200).send("Ok");
 };
 
-export const refresh = async (req, res, next) => {
+export const refresh = async (req, res) => {
 	const { accessToken, id } = req;
 	const refreshToken = req.body.refreshToken;
 
 	const auth = await Auth.findOne({ where: { refreshToken } });
-	if (!auth) {
-		next(new AuthorizationError());
-		return;
-	}
+	if (!auth) throw new AuthorizationError();
+
 	if (auth.accessToken !== accessToken) {
 		auth.destroy();
-		next(new AuthorizationError());
-		return;
+		throw new AuthorizationError();
 	}
 	auth.accessToken = createToken({ id }, EXPIRES_ACCESS);
 	auth.save();
