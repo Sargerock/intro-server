214ae852619ed7d701e702b85aeec63948f9a10b
sargerock <sargerock.ua@gmail.com>
2020-04-08 18:43:39 +0300

    find-user route

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+
 import { User } from "../models";
 import { HandledError } from "../utils/errors";
 
@@ -8,4 +10,15 @@ export const getUser = async (req, res) => {
 	if (!user) throw new HandledError("User not found.", 404);
 
 	res.status(200).json(user);
+};
+
+export const findUsers = async (req, res) => {
+	const userName = req.params.username;
+
+	const users = await User.findAll({
+		where: { userName: { [Op.like]: `${userName}%` } },
+		limit: 5,
+	});
+
+	res.status(200).json(users);
 };
--- routers/users-router.js
+++ routers/users-router.js
@@ -1,10 +1,11 @@
 import router from "express-promise-router";
 import authGuard from "../middleware/auth-guard";
-import { getUser } from "../controllers/users-controller";
+import { getUser, findUsers } from "../controllers/users-controller";
 
 // /api/users
 const route = router();
 
+route.get("/find/:username", authGuard, findUsers);
 route.get("/:username", authGuard, getUser);
 
 export default route;
